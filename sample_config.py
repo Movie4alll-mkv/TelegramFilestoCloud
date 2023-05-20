faa769cba1b96c29e40aa3d355bf2ee010e20a6e
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2023-05-20 12:30:19 +0500

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,6 @@
 class Config:
     BOT_USE = False
-    BOT_TOKEN = ''                         # from @botfather
-    APP_ID =                               # from https://my.telegram.org/apps
-    API_HASH = ''                          # from https://my.telegram.org/apps
-    AUTH_USERS = []
+    BOT_TOKEN = '5541380668:AAGH9yGr_4leFAgG35raDvAi1Q82ynMJGG0'                         # from @botfather
+    APP_ID =   6534707                            # from https://my.telegram.org/apps
+    API_HASH = '4bcc61d959a9f403b2f20149cbbe627a'                          # from https://my.telegram.org/apps
+    AUTH_USERS = [1430593323]
